92f338e4348c84baeed0a9cdaedcb52ab5da1804
andreas_siedler <andreas@waytation.com>
2019-12-07 10:00:14 +0100

    Improvement of ContactForm Validation

--- validator/contactForm.js
+++ validator/contactForm.js
@@ -1,16 +1,16 @@
 exports.contactFormValidator = (req, res, next) => {
-    req.check("name", "Name is required!").notEmpty();
+    req.check("name", "Please enter a name!").notEmpty();
     req.check("email", "Email must be between 3 to 32 characters")
         .matches(/.+\@.+\..+/)
-        .withMessage("Email must contain @")
+        .withMessage("Please enter a valid Email Address!")
         .isLength({
             min: 4,
             max: 32
         });
-    req.check("message", "Password is required").notEmpty();
+    req.check("message", "Please enter a Message!").notEmpty();
     req.check("message")
         .isLength({ min: 20 })
-        .withMessage("Message must contain at least 20 characters");
+        .withMessage("Message must contain at least 20 characters!");
     const errors = req.validationErrors();
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
