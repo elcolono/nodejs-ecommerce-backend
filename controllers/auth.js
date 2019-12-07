366b6d06d704d83f5ce00fc72676293fc722f01c
andreas_siedler <andreas@waytation.com>
2019-12-07 07:13:49 +0100

    token expiry update

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,13 +41,15 @@ exports.signin = (req, res) => {
                 error: "Email and password dont match"
             });
         }
-        // generate a signed token with user id and secret
-        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-        // persist the token as 't' in cookie with expiry date
-        res.cookie("t", token, { expire: new Date() + 9999 });
-        // return response with user and token to frontend client
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '3600' });
+        res.cookie('token', token, { expiresIn: '3600' });
+
         const { _id, firstname, lastname, email, role } = user;
-        return res.json({ token, user: { _id, firstname, lastname, email, role } });
+        return res.json({
+            token,
+            user: { _id, firstname, lastname, email, role },
+            expiresIn: 3600
+        });
     });
 };
 
